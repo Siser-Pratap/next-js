ef2b11fbad4cec4ba69a1d8cd48e54ddd99ed6f2
Siser Pratap <siserinsevoc@gmail.com>
2024-09-11 10:29:36 +0530

    updated

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -44,7 +44,7 @@ export default function ProfilePage(){
         <h1>Profile</h1>
         <hr />
         <p>Profile page</p>
-        <h2 className="p-1 rounded bg-green-500">{data === "Nothing" ? "User not logged in" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+        <h2 className="p-1 rounded bg-green-500">{data === "Nothing" ? "User Id" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
         <button
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
